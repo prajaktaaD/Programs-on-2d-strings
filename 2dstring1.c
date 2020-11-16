e1a10221a2990f4ad1b6fc709f00b6a1087b701b
prajaktaaD <70463406+prajaktaaD@users.noreply.github.com>
2020-11-16 19:47:25 +0530

    Create 2dstring1.c
    WAP a program to scan 2d char array::

---
+++ 2dstring1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+
+void main()
+{
+  int i,j;
+  char str[5][15];
+
+  printf("Enter 5 strings::\n");
+  for(i=0;i<;i++)
+  scanf("%s",str[i]);
+
+  printf("\nDisplay:\n");
+  for(i=0;i<5;i++)
+  printf("%s\n",str[i]);
+}
